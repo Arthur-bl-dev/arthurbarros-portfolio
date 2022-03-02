331b88fa4d38455cf1d713d9f72a99b785c9c300
Arthur BL <arthurbldev@gmail.com>
2022-03-02 01:13:51 -0300

    added news styles projects

--- components/myPortfolio.tsx
+++ components/myPortfolio.tsx
@@ -25,64 +25,46 @@ const listProjects: ImagesListType = [
         src: '/images/projects/LP_Example_1.png',
         loading: 'lazy',
         alt: 'Prototype of a website for a company.',
-        width: 1440,
-        height: 1024
     },
     {
-        src: '/images/projects/SuperGeeks.png',
+        src: '/images/projects/LP_SuperGeeks_1.png',
         loading: 'lazy',
         alt: 'SuperGeeks - a company that provides a platform for childrens,youngs and adults to learn about the latest technologies and trends in the industry.',
-        width: 1920,
-        height: 2916
     },
     {
-        src: '/images/projects/Franquias_SuperGeeks.png',
+        src: '/images/projects/LP_SuperGeeks_2_Franquias.png',
         loading: 'lazy',
         alt: 'SuperGeeks - This time a static page for your trade networks of franchise',
-        width: 1920,
-        height: 5292
     },
     {
         src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
         loading: 'lazy',
         alt: 'Under Construction',
-        width: 1920,
-        height: 2916
     },
     {
         src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
         loading: 'lazy',
         alt: 'Under Construction',
-        width: 1920,
-        height: 2916
     },
     {
         src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
         loading: 'lazy',
         alt: 'Under Construction',
-        width: 1920,
-        height: 2916
     },
     {
         src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
         loading: 'lazy',
         alt: 'Under Construction',
-        width: 1920,
-        height: 2916
     },
     {
         src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
         loading: 'lazy',
         alt: 'Under Construction',
-        width: 1920,
-        height: 2916
     },
     {
         src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
-        loading: 'lazy',
         alt: 'Under Construction',
-        width: 1920,
-        height: 2916
+        loading: 'lazy',
     },
 ]
 
@@ -90,24 +72,48 @@ const MyPortfolio: NextPage = () => {
     const [currentImageIndex, setCurrentIndex] = useState(-1);
     const [isOpen, setIsOpen] = useState(false);
 
-
+    const listCapeProjects = useRef([
+        {
+            src: '/images/projects/cape_LP_Example_1.png',
+        },
+        {
+            src: '/images/projects/cape_LP_Example_2.png',
+        },
+        {
+            src: '/images/projects/cape_LP_Example_3.png',
+        },
+        {
+            src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
+        },
+        {
+            src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
+        },
+        {
+            src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
+        },
+        {
+            src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
+        },
+        {
+            src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
+        },
+        {
+            src: 'https://via.placeholder.com/1920x2916/18191c/9c9e9e.jpeg?text=Under+construction',
+        },
+    ])
 
     const RenderProjects = useCallback(() => (
 
         <>
             {
-                listProjects.map((project, index) => (
+                listCapeProjects.current.map((project, index) => (
                     <div onClick={() => {
                         setCurrentIndex(index);
                         setIsOpen(true);
                     }} key={index} className={styles.itemProject}>
                         <div className={styles.imageBackground}>
-                            <Image src={project.src} alt={project.alt} width="450px" height="450px" objectFit="cover" />
+                            <Image src={project.src} alt={`Project - ${index}`} width="450px" height="450px" objectFit="cover" />
                         </div>
-                        <div className={styles.contentName}>
-                            <h2 hidden>{project.alt}</h2>
-                        </div>
-
                     </div>
                 ))
             }
